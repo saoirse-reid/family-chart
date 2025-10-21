5aa937fdb88ed17bb3fd98b8f9f780cf09a0decd
Saoirse <saoirse.reid@hotmail.com>
2025-10-21 15:56:58 -0700

    Remove some build checks

--- prePublish.js
+++ prePublish.js
@@ -1,16 +1,4 @@
-import { execSync, spawn } from 'child_process';
-
-console.log('Deploying documentation...................');
-execSync('yarn run docs:deploy', { stdio: 'inherit' });
+import { execSync } from 'child_process';
 
 console.log('Building...................');
 execSync('yarn run build', { stdio: 'inherit' });
-
-console.log('Running dev................');
-const runDev = spawn('yarn', ['run', 'dev', '--port', '8080'], { stdio: 'inherit' });
-
-console.log('Running test-run.............');
-execSync('yarn run test-run', { stdio: 'inherit' });
-
-console.log('Killing dev................');
-runDev.kill();
